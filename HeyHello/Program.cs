1bbf375ee975ac54ae3a48f584be63cec6fb94bf
white <white@example.com>
2025-11-01 16:12:18 +0500

    method greetWhite

--- HeyHello/Program.cs
+++ HeyHello/Program.cs
@@ -2,6 +2,11 @@
 
 public static class Hello
 {
+    static void GreetWhite()
+    {
+        Console.WriteLine("hello white");
+    }
+
     static void Main()
     {
         Console.WriteLine("hello");
